945ae145f2e0ae8d281558bb9443a6c157433794
mukeshrai32 <mukeshrai3449@gmail.com>
2024-08-10 11:14:05 +0545

    #Update devdojo/auth UI add

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Devdojo\Auth\Models\User as AuthUser;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends AuthUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
